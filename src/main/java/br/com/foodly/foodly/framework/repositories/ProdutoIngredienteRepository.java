8e28098cd4665b28f33890ee28bf51d18e8fbab5
Murillo Morgado <mumorgado@hotmail.com>
2025-10-06 18:26:10 -0300

    Implementando a query para deletar todos registros a partir de um idProduto

--- src/main/java/br/com/foodly/foodly/framework/repositories/ProdutoIngredienteRepository.java
+++ src/main/java/br/com/foodly/foodly/framework/repositories/ProdutoIngredienteRepository.java
@@ -1,9 +1,14 @@
 package br.com.foodly.foodly.framework.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 
 import br.com.foodly.foodly.framework.models.ProdutoIngrediente;
+import jakarta.transaction.Transactional;
 
-public interface ProdutoIngredienteRepository extends JpaRepository<ProdutoIngrediente, Long>{
-    
+public interface ProdutoIngredienteRepository extends JpaRepository<ProdutoIngrediente, Long> {
+
+    @Modifying
+    @Transactional
+    void deleteByProdutoId(Long idProduto);
 }
